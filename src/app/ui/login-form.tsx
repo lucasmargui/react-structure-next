9bcef3635648d955c3ab5d00f459474b25d07cc4
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-03-26 11:45:59 -0300

    update: add login

--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -5,6 +5,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import Link from 'next/link';
 
 export default function LoginForm() {
   return (
@@ -65,8 +66,18 @@ export default function LoginForm() {
 
 function LoginButton() {
   return (
+    <Link
+       
+      href="/dashboard"
+      >
     <Button className="mt-4 w-full">
+
+      
+      
+ 
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
+
+    </Link>
   );
 }
